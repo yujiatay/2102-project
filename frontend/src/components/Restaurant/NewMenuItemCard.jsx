df7fb0b3e8720f215563a299345ec35191601588
John Phua <phuajohn0@gmail.com>
2019-11-06 11:59:13 +0800

    Add menu items for restaurant profile

---
+++ frontend/src/components/Restaurant/NewMenuItemCard.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Card, CardBody,
+  CardTitle, Button, FormGroup, Label, Input, Form
+} from 'reactstrap';
+
+const NewMenuItemCard = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle>New Menu Item Details</CardTitle>
+          <Form>
+            <FormGroup row>
+              <Label for="itemName">Item Name</Label>
+              <Input
+                id="name"
+                type="text"
+                defaultValue={"Waffles with Chicken"}
+              />
+            </FormGroup>
+            <FormGroup row>
+              <Label for="itemType">Type</Label>
+              <Input
+                id="itemType"
+                defaultValue={"Main"}
+              />
+            </FormGroup>
+            <FormGroup row>
+              <Label for="itemPrice">Price</Label>
+              <Input
+                id="itemPrice"
+                type="number"
+                defaultValue={3.14}
+              />
+            </FormGroup>
+            <FormGroup row>
+              <Label for="itemDescription">Description</Label>
+              <Input
+                id="itemDescription"
+                type="textarea"
+                defaultValue={"The only other acceptable option."}
+              />
+            </FormGroup>
+            <FormGroup row>
+              <Label for="image">Link for Image</Label>
+              <Input
+                id="image"
+                type="text"
+                defaultValue={"https://via.placeholder.com/150"}
+              />
+            </FormGroup>
+            <Button block>Confirm</Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default NewMenuItemCard;
--- frontend/src/views/pages/Restaurant/RestaurantProfile.jsx
+++ frontend/src/views/pages/Restaurant/RestaurantProfile.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import {
   Col, Container, Row,
-  FormGroup, InputGroup, Label,
-  InputGroupAddon, InputGroupText, Input, Form, Button
+  FormGroup, Label,
+  Input, Form, Button,
 } from 'reactstrap';
 
 import Navbar from "components/Navbars/DarkNavBarRestaurant";
-import ReactDatetime from "react-datetime";
+import MenuItemCard from "components/Restaurant/MenuItemCard";
+import NewMenuItemCard from "components/Restaurant/NewMenuItemCard";
 
 class RestaurantProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: null,
-      modal: false,
-      activeTab: '1'
+      newItem: false,
     }
   }
 
-  switchTab = (newTab) => {
-    if (newTab !== this.state.activeTab) {
-      this.setState({ activeTab: newTab });
-    }
+  openNewItemForm = () => {
+    this.setState({newItem: true});
+  };
+
+  closeNewItemForm = () => {
+    this.setState({newItem: false});
   };
 
   componentDidMount() {
@@ -31,69 +32,94 @@ class RestaurantProfile extends React.Component {
   }
 
   render() {
+    const newItem = this.state.newItem;
     return (
       <>
         <Navbar/>
         <main ref="main">
-          <section className="section h-100vh">
+          <section className="section">
+            <Container className="pt-md align-content-md-center">
+              <Row className="justify-content-md-center">
+                <Col xs="10">
+                  <p className="h1">Restaurant Details</p>
+                  <Form>
+                    <FormGroup row>
+                      <Label for="name">Restaurant Name</Label>
+                      <Input
+                        id="name"
+                        type="text"
+                        value={"ABC Foodings"}
+                      />
+                    </FormGroup>
+                    <FormGroup row>
+                      <Label for="cuisine">Cuisine Type</Label>
+                      <Input type="select" name="select" id="cuisine">
+                        <option value={1}>Bakery</option>
+                        <option value={2}>Dessert</option>
+                        <option value={3}>Fast Food</option>
+                        <option value={4}>Vegetarian</option>
+                        <option value={11}>American</option>
+                        <option value={12}>Chinese</option>
+                        <option value={13}>French</option>
+                        <option value={14}>Indian</option>
+                        <option value={15}>Indonesia</option>
+                        <option value={16}>Italian</option>
+                        <option value={17}>Japanese</option>
+                        <option value={18}>Korean</option>
+                        <option value={19}>Mexican</option>
+                        <option value={20}>Middle Eastern</option>
+                        <option value={21}>Thai</option>
+                        <option value={22}>Vietnamese</option>
+                        <option value={23}>Western</option>
+                      </Input>
+                    </FormGroup>
+                    <FormGroup row>
+                      <Label for="address">Address</Label>
+                      <Input
+                        id="address"
+                        type="text"
+                        value={"8 Club Street S123456"}
+                      />
+                    </FormGroup>
+                    <FormGroup row>
+                      <Label for="opening">Opening Hours</Label>
+                      <Input
+                        id="opening"
+                        type="textarea"
+                        value={"Monday to Friday 8am - 8pm"}
+                      />
+                    </FormGroup>
+                    <FormGroup row>
+                      <Label for="capacity">Maximum Capacity</Label>
+                      <Input
+                        id="capacity"
+                        type="number"
+                        value={1}
+                      />
+                    </FormGroup>
+                    <Button block>Edit</Button>
+                  </Form>
+                </Col>
+              </Row>
+            </Container>
             <Container className="pt-md">
-                <Form>
-                  <FormGroup row>
-                    <Label for="name">Restaurant Name</Label>
-                    <Input
-                      id="name"
-                      type="text"
-                      value={"ABC Foodings"}
-                    />
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="cuisine">Cuisine Type</Label>
-                    <Input type="select" name="select" id="cuisine">
-                      <option value={1}>Bakery</option>
-                      <option value={2}>Dessert</option>
-                      <option value={3}>Fast Food</option>
-                      <option value={4}>Vegetarian</option>
-                      <option value={11}>American</option>
-                      <option value={12}>Chinese</option>
-                      <option value={13}>French</option>
-                      <option value={14}>Indian</option>
-                      <option value={15}>Indonesia</option>
-                      <option value={16}>Italian</option>
-                      <option value={17}>Japanese</option>
-                      <option value={18}>Korean</option>
-                      <option value={19}>Mexican</option>
-                      <option value={20}>Middle Eastern</option>
-                      <option value={21}>Thai</option>
-                      <option value={22}>Vietnamese</option>
-                      <option value={23}>Western</option>
-                    </Input>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="address">Address</Label>
-                    <Input
-                      id="address"
-                      type="text"
-                      value={"8 Club Street S123456"}
-                    />
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="opening">Opening Hours</Label>
-                    <Input
-                      id="opening"
-                      type="textarea"
-                      value={"Monday to Friday 8am - 8pm"}
-                    />
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="capacity">Maximum Capacity</Label>
-                    <Input
-                      id="capacity"
-                      type="number"
-                      value={1}
-                    />
-                  </FormGroup>
-                  <Button>Edit</Button>
-                </Form>
+              <Row className="justify-content-md-center">
+                <Col xs="10">
+                  <p className="h1">Menu Items</p>
+                  {
+                    newItem
+                    ? <Button onClick={this.closeNewItemForm} block>Cancel</Button>
+                      : <Button onClick={this.openNewItemForm} block>Add new item</Button>
+                  }
+                  {
+                    newItem &&
+                    <NewMenuItemCard/>
+                  }
+                  <MenuItemCard/>
+                  <MenuItemCard/>
+                  <MenuItemCard/>
+                </Col>
+              </Row>
             </Container>
           </section>
         </main>
