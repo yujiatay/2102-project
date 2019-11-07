b32093ca9de090f2559a7199d58ed008240011b4
sharan8 <sharan.t@u.nus.edu>
2019-11-08 00:41:07 +0800

    Update ArticleCard with real data

--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -3,20 +3,34 @@ import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
+import { Redirect } from 'react-router-dom';
 
-const ArticleCard = (props) => {
-  return (
-    <div>
-      <Card>
-        <CardBody>
-          <CardTitle><a href="/articleView">Article Title</a></CardTitle>
-          <CardSubtitle>Posted By: John</CardSubtitle>
-          <CardText>Posted on: 00:00:00 16 Aug 2019</CardText>
-          <CardText>Number of comments: 3</CardText>
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
+class ArticleCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  goToArticle() {
+    return <Redirect to='/'/>
+  }
+
+  render() {
+    const createdTimestamp = new Date(this.props.createdAt).toString();
+    return (
+      <>
+        <div>
+          <Card>
+            <CardBody>
+              <CardTitle><a href={"/articles/" + this.props.username + "/" + this.props.createdAt}>{this.props.title}</a></CardTitle>
+              <CardSubtitle>Posted By: {this.props.username}</CardSubtitle>
+              <CardText>Posted on: {createdTimestamp}</CardText>
+              <Button onClick={this.goToArticle}>View Article</Button>
+            </CardBody>
+          </Card>
+        </div>
+      </>
+    )
+  }
+}
 
 export default ArticleCard;
