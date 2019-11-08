973f6de9d0d06b48f8f0924822983a0b670b7089
John Phua <phuajohn0@gmail.com>
2019-11-08 17:00:24 +0800

    Add modal to menu item delete

--- frontend/src/components/Restaurant/MenuItemCard.jsx
+++ frontend/src/components/Restaurant/MenuItemCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   Card, CardText, CardBody,
   CardTitle, CardSubtitle,
-  Col, Row, Button
+  Col, Row, Button, Modal
 } from 'reactstrap';
 
 import { menuItemTypes } from "../../constants";
@@ -11,17 +11,56 @@ import http from "../../http";
 class MenuItemCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      deleteModal: false
+    }
   }
 
   submitDelete(name) {
-    return () =>
-    this.props.onCallback(http.delete(`/restaurants/${this.props.restaurant.username}/menuitems/${name}`));
+    return () => {
+      this.toggleModal();
+      this.props.onCallback(http.delete(`/restaurants/${this.props.restaurant.username}/menuitems/${name}`));
+    }
+  }
+
+  toggleModal() {
+    this.setState({
+      deleteModal: !this.state.deleteModal
+    })
   }
 
   render() {
     const { name, description, type, price, image } = this.props.item;
+    const { deleteModal } = this.state;
     return (
-      <div>
+      <>
+        <Modal
+          isOpen={deleteModal}
+          onClose={this.toggleModal}
+        >
+          <div className="modal-header">
+            <h5 className="modal-title" id="modalLabel">
+              Are you sure?
+            </h5>
+            <button
+              aria-label="Close"
+              className="close"
+              data-dismiss="modal"
+              type="button"
+              onClick={this.toggleModal}
+            >
+              <span aria-hidden={true}>×</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            You are deleting {name}
+          </div>
+          <div className="modal-footer">
+            <Button color="danger" type="button" onClick={this.submitDelete(name)}>
+              Confirm
+            </Button>
+          </div>
+        </Modal>
         <Card>
           <CardBody>
             <Row>
@@ -33,13 +72,12 @@ class MenuItemCard extends React.Component {
                 <CardSubtitle>{menuItemTypes[type]}</CardSubtitle>
                 <CardText>Price: ${price}</CardText>
                 <CardText>{description}</CardText>
-                <Button color="primary">Edit</Button>
-                <Button color="warning" onClick={this.submitDelete(name)}>Remove</Button>
+                <Button color="warning" onClick={this.toggleModal.bind(this)}>Remove</Button>
               </Col>
             </Row>
           </CardBody>
         </Card>
-      </div>
+      </>
     );
   };
 }
