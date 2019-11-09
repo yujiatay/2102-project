73a4a85dc3e94ecb1a41d066c0a451ccacec3db5
Yu Jia Tay <yujiatay@gmail.com>
2019-11-09 23:31:03 +0800

    Add charts for analytics

--- frontend/src/views/pages/Restaurant/Analytics.jsx
+++ frontend/src/views/pages/Restaurant/Analytics.jsx
@@ -13,11 +13,13 @@ import {
   Label,
   Row
 } from 'reactstrap';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 import Navbar from "components/Navbars/DarkNavBarRestaurant";
 import ReactDatetime from "react-datetime";
 import { requireAuthentication } from "components/AuthenticatedComponent";
 import http from "http.js";
+import { getCurrentUnixTimestamp, getUnixTimestamp } from "utils.js";
 
 class Analytics extends React.Component {
   constructor(props) {
@@ -42,6 +44,8 @@ class Analytics extends React.Component {
       min: startDate || ReactDatetime.moment().subtract(1, 'month').valueOf(),
       max: endDate || Date.now(),
     };
+    dateQuery.min = dateQuery.min.getTime();
+    dateQuery.max = dateQuery.max.getTime();
 
     const timeslotResponse = await http.get(`/analytics/popularity/timeslot`, {params: dateQuery});
     const dateResponse = await http.get(`/analytics/popularity/date`, {params: dateQuery});
@@ -73,6 +77,51 @@ class Analytics extends React.Component {
         [key]: date.toDate()
       })
     }
+  }
+
+  renderTimeslotPopularity = () => {
+    let data = this.state.timeSlotPopularity;
+    let dic = {};
+    for (let i = 0; i < data.length; i++) {
+      let label = data[i].startTime;
+      let count = data[i].bookings + data[i].confirmedBookings;
+      if (label in dic) {
+        dic[label] += count;
+      } else {
+        dic[label] = count;
+      }
+    }
+    let chartData = []
+    for (let key in dic) {
+      chartData.push({ "label": key, "count": dic[key]});
+    }
+    chartData.sort((a, b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0));
+
+    return (
+      <LineChart width={600} height={300} data={chartData}>
+        <Line type="monotone" dataKey="count" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="label" />
+        <YAxis />
+      </LineChart>
+    )
+  }
+
+  renderDatePopularity = () => {
+    let data = this.state.datePopularity;
+    for (let i = 0; i < data.length; i++) {
+      data[i].label = new Date(data[i].date).toLocaleDateString();
+      data[i].count = data[i].bookings + data[i].confirmedBookings;
+    }
+    data.sort((a, b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0));
+    return (
+      <LineChart width={600} height={300} data={data}>
+        <Line type="monotone" dataKey="count" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="label" />
+        <YAxis />
+      </LineChart>
+    )
   }
 
   render() {
@@ -140,13 +189,13 @@ class Analytics extends React.Component {
               <Row className="justify-content-md-center">
                 <Col>
                 <p className="h3">Popularity by Timeslots</p>
-                {"INSERT CHART HERE"}
+                {this.renderTimeslotPopularity()}
                 </Col>
               </Row>
               <Row className="justify-content-md-center">
                 <Col>
                 <p className="h3">Popularity by Date</p>
-                {"INSERT CHART HERE"}
+                {this.renderDatePopularity()}
                 </Col>
               </Row>
             </Container>
